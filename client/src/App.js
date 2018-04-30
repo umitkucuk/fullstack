c2b79f8894324b0a3a0592f33e0f3fda3d3d8a9b
Ümit Küçük <umitkucuk@users.noreply.github.com>
2018-04-30 04:53:01 +0300

    new client

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, { Component, Fragment } from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const QUERY = gql`
+  query authHello {
+    authHello
+  }
+`
+
+class App extends Component {
+  render() {
+    return (
+      <Query query={QUERY}>
+        {({ data, error, loading }) => {
+          if (error) return 'Something is wrong :('
+          if (loading) return 'Loading...'
+
+          return (
+            <Fragment>
+              <h1>{data.authHello}</h1>
+            </Fragment>
+          )
+        }}
+      </Query>
+    )
+  }
+}
+
+export default App
