a70815862120cb307ffd0d8dc7a68f44360723f4
Ümit Küçük <umittkucukk@gmail.com>
2019-06-27 16:34:06 +0300

    edit

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,78 +1,74 @@
-import * as express from "express";
-import { ApolloServer } from "apollo-server-express";
-import * as helmet from "helmet";
-import * as cors from "cors";
-import * as session from "express-session";
-import * as mongoose from "mongoose";
-(<any>mongoose).Promise = require("bluebird");
-import * as mongoSessionStore from "connect-mongo";
-import * as errorhandler from "errorhandler";
+import * as express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import * as helmet from 'helmet'
+import * as cors from 'cors'
+import * as session from 'express-session'
+import * as mongoose from 'mongoose'
+;(<any>mongoose).Promise = require('bluebird')
+import * as mongoSessionStore from 'connect-mongo'
+import * as errorhandler from 'errorhandler'
 
-import config from "./config/app.config";
-import typeDefs from "./graphql/typedefs";
-import resolvers from "./graphql/resolvers";
+import config from './config/app.config'
+import typeDefs from './graphql/typedefs'
+import resolvers from './graphql/resolvers'
 
-const PORT = config.PORT || 8000;
+const PORT = config.PORT || 8000
 
-const app = express();
+const app = express()
 
-app.use(helmet());
+app.use(helmet())
 
 mongoose
   .connect(config.MONGO_URI, { useCreateIndex: true, useNewUrlParser: true })
-  .then(() => console.log("Connection succesful to DB!"))
-  .catch(err => console.log(err));
+  .then(() => console.log('Connection succesful to DB!'))
+  .catch(err => console.log(err))
 
 // Error Handler
-if (config.NODE_ENV === "development") {
-  app.use(errorhandler());
+if (config.NODE_ENV === 'development') {
+  app.use(errorhandler())
 }
 
 // Cors Middleware Options
 const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true
-};
+  origin: 'http://localhost:3000',
+  credentials: true,
+}
 
-const MongoStore = mongoSessionStore(session);
+const MongoStore = mongoSessionStore(session)
 
 app.use(
   session({
-    name: "sid",
-    secret: config.NODE_ENV === "production" ? config.SESSION_SECRET : "secret",
+    name: 'sid',
+    secret: config.NODE_ENV === 'production' ? config.SESSION_SECRET : 'secret',
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
-      ttl: 14 * 24 * 60 * 60
+      ttl: 14 * 24 * 60 * 60,
     }),
     resave: false,
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
       secure: false,
-      maxAge: 14 * 24 * 60 * 60 * 1000
-    }
-  })
-);
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+    },
+  }),
+)
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: ({ req, res }) => ({
     req,
-    res
+    res,
   }),
   playground: {
     settings: {
-      "editor.theme": "dark",
-      "request.credentials": "include"
-    }
-  }
-});
+      'editor.theme': 'dark',
+      'request.credentials': 'include',
+    },
+  },
+})
 
-server.applyMiddleware({ app, cors: corsOptions });
+server.applyMiddleware({ app, cors: corsOptions })
 
-app.listen(PORT, () =>
-  console.log(
-    `🚀  Server ready at http://localhost:${PORT}${server.graphqlPath}`
-  )
-);
+app.listen(PORT, () => console.log(`🚀  Server ready at http://localhost:${PORT}${server.graphqlPath}`))
